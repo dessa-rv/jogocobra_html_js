90a90740a0e74d4a6d31b04e04b045ca66af2410
andressa <andressavieira7@hotmail.com>
2021-02-21 12:09:48 -0300

    aleatoriedade da comida, tamanho da cobrinha e o fim do jogo

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function criarBG(){
 
 }
 
- function criariCobrinha(){
+ function criarCobrinha(){
     for(i=0; i < snake.length;i++ ){
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
@@ -44,14 +44,22 @@ function update(event){
 
 
  function iniciarJogo(){
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     
 
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+    
+    for(i= 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over =(');
+        }
+    }
     criarBG();
-    criariCobrinha();
+    criarCobrinha();
     drawFood();
 
     
@@ -63,7 +71,12 @@ function update(event){
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); 
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
 
     let newHead = {
         x: snakeX,
